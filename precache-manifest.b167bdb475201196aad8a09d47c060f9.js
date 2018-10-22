1a0fe4de3fb0bd9e0e67139d1455c1cfc04defaf
Petra <petragerzova@gmail.com>
2018-10-22 21:26:38 +0100

    Updates

---
+++ precache-manifest.b167bdb475201196aad8a09d47c060f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec0b4348381f9f0a6531ab06131cb0b6",
+    "url": "/star-wars/static/media/star-wars.ec0b4348.jpg"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/star-wars/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "48e867e8c4414f7eb271",
+    "url": "/star-wars/static/js/runtime~main.48e867e8.js"
+  },
+  {
+    "revision": "75b8cfa2d97e66aed22c",
+    "url": "/star-wars/static/js/main.75b8cfa2.chunk.js"
+  },
+  {
+    "revision": "a3c4a255946a87985ad4",
+    "url": "/star-wars/static/js/1.a3c4a255.chunk.js"
+  },
+  {
+    "revision": "75b8cfa2d97e66aed22c",
+    "url": "/star-wars/static/css/main.9a781a43.chunk.css"
+  },
+  {
+    "revision": "a3c4a255946a87985ad4",
+    "url": "/star-wars/static/css/1.bc5a7277.chunk.css"
+  },
+  {
+    "revision": "97bd6873206f0fa5d7fe7816b088df9b",
+    "url": "/star-wars/index.html"
+  }
+];
